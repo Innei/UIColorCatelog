f4b673bd5cd2a2d2ccb80ae0f171e8286a3ceefa
Innei <tukon479@gmail.com>
2025-01-12 12:41:49 +0800

    Initial Commit

---
+++ ColorCatelog/ColorCatelogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCatelogApp.swift
+//  ColorCatelog
+//
+//  Created by Innei on 2025/1/12.
+//
+
+import SwiftUI
+
+@main
+struct ColorCatelogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
